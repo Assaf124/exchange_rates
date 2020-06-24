00cac20bd4b555b99cd4e59035d5ca2c4550cbfb
assafa <assaf242424@gmail.com>
2020-06-24 12:02:47 +0300

    re-write the 'print' command in the ert program

--- ert.py
+++ ert.py
@@ -4,12 +4,12 @@ import exrates
 exrt = exrates.Exrates()
 exrt.get_currencies()
 # user_input = input("Please type in a date in the format of YYYY-MM-DD: ")
-user_input = '2007-03-12'
+user_input = '2007-03-14'
 
 exrt.get_exrates(user_input)
 # print(exrt.exchange_rates)
 
 for key, value in exrt.currencies.items():
     if value in exrt.exchange_rates.keys():
-        # print(f'{value} ({key})   {exrt.exchange_rates[value]}')
-        print('{:41s} ({}) {:2s} {}'.format(key, value, '|', exrt.exchange_rates[value]))
+        print(f'{key:41s}  {value:2s}  |  {exrt.exchange_rates[value]}')
+        # print('{:41s} ({}) {:2s} {}'.format(key, value, '|', exrt.exchange_rates[value]))
